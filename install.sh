74fe92df25cc1117d29565c6bf644bd92154bc4a
root <root@IOCore.OEL6U3.BuildMachine>
2013-01-21 19:26:29 +0800

    fix typo

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ die(){
 cd ~
 
 #Checkout my config
-#git clone git@github.com:Swind/linux-config.git "$CONFIG_HOME"
+git clone git@github.com:Swind/linux-config.git "$CONFIG_HOME"
 
 ln -s $CONFIG_HOME/.vim .vim
 ln -s .vim/.vimrc .vimrc
@@ -23,10 +23,10 @@ ln -s $CONFIG_HOME/.zshrc .zshrc
 ln -s $CONFIG_HOME/.tmux.conf .tmux.conf
 
 #install vundle
-#git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 #install oh-my-zsh
-#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 echo "Configuration files has been installed."
 cd "$CONFIG_HOME"
