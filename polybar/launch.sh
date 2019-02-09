2caa85684064f7bdb475f8fb906c9ec4c1776f26
Swind <swind@cloudmosa.com>
2019-02-09 15:22:43 +0800

    Support multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example &
-polybar example2 &
+polybar primary &
+polybar second &
 
 echo "Bars launched..."
