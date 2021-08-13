573e79fb31b355c36ef3bfb054aac1d68b4ffcd8
Swind <swind@cloudmosa.com>
2021-08-14 01:59:14 +0800

    Add fonts install script and change the neovim repository from stable to unstable

---
+++ scripts/font.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+mkdir -p $HOME/.fonts
+mv *.ttf $HOME/.fonts
+fc-cache -f -v
--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
