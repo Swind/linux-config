b325b614a19c383dcf23fd65eb4a5638a5bf6c47
Swind <swind@cloudmosa.com>
2023-03-14 15:35:01 +0800

    scripts: fix “Authentication is required to create a color profile/managed device” on Ubuntu 22.04 / 20.04

---
+++ scripts/fix-authentication_is_required_to_create_a_color_profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Reference: https://devanswers.co/how-to-fix-authentication-is-required-to-create-a-color-profile-managed-device-on-ubuntu-20-04-20-10/
+cat <<EOF > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
