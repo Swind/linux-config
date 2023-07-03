3e4ff275d2debe85f61e50691f235eb3cbf0f683
Swind <swind@cloudmosa.com>
2023-07-03 16:11:02 +0800

    Add setup script

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -2,7 +2,7 @@
 ZSHRC_PATH=$HOME/.config/zsh/.zshrc
 CURRENT_DIR=$(dirname "$SCRIPT")
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 sed -i '/### RUST BLOCK START ###/,/### RUST BLOCK END ###/d' $ZSHRC_PATH
 cat << 'EOF' >> $ZSHRC_PATH
