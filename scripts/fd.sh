f43a46bd087b548eb0dac2d4ca88c177c477a6c8
Swind <swind@cloudmosa.com>
2023-11-12 05:59:25 +0800

    Add a install script for fg

--- scripts/fd.sh
+++ scripts/fd.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+ZSHRC_PATH=$HOME/.config/zsh/.zshrc
+CURRENT_DIR=$(dirname "$SCRIPT")
+
 cargo install fd-find
+
+sed -i '/### FD BLOCK START ###/,/### FD BLOCK END ###/d' $ZSHRC_PATH
+cat << 'EOF' >> $ZSHRC_PATH
+### FD BLOCK START ###
+export FZF_DEFAULT_COMMAND="fd --type file --color=always"
+export FZF_DEFAULT_OPTS="--ansi"
+### FD BLOCK END ###
+EOF
