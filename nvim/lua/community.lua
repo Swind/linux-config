7f1f300c95ba439211dabfbfaae607e2ae315a81
Swind <swind@cloudmosa.com>
2024-04-04 09:23:22 +0000

    Upgrade nvim to 4.0

---
+++ nvim/lua/community.lua
@@ -0,0 +1,13 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.rust" },
+  -- import/override with your plugins folder
+}
