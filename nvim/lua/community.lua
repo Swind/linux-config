b59d778f5f4c9f555198ac4ed1e4363e04cf64a6
Swind <swind@cloudmosa.com>
2024-04-04 13:49:46 +0000

    Add more astronvim community plugins for go, python, rust

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,8 +6,19 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
+  -- Pack
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  -- Completion
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
   -- import/override with your plugins folder
 }
