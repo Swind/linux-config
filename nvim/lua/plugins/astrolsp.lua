6b39e9078cc02ca3ec3c6c240eb1c1260e75c459
swind <swind@cloudmosa.com>
2024-05-06 20:00:32 +0800

    gopls: ignore shadow: declaration of err shadows declaration ...

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -47,6 +47,51 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              ST1003 = true,
+              fieldalignment = false,
+              fillreturns = true,
+              nilness = true,
+              nonewvars = true,
+              shadow = false,
+              undeclaredname = true,
+              unreachable = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            codelenses = {
+              gc_details = true, -- Show a code lens toggling the display of gc's choices.
+              generate = true, -- show the `go generate` lens.
+              regenerate_cgo = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            buildFlags = { "-tags", "integration" },
+            completeUnimported = true,
+            diagnosticsDelay = "500ms",
+            matcher = "Fuzzy",
+            semanticTokens = true,
+            staticcheck = true,
+            symbolMatcher = "fuzzy",
+            usePlaceholders = true,
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
