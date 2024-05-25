6476bd58f38d3b9ebe137f4488faf85b36bf0f0b
swind <swind@cloudmosa.com>
2024-05-25 16:14:07 +0800

    Upgrade to neovim 0.10.0

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -4,15 +4,20 @@ sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automak
 
 TARGET_DIR=$HOME/Software
 NEOVIM_DIR=$TARGET_DIR/neovim
+NEOVIM_VERSION=v0.10.0
 
 mkdir -p $TARGET_DIR
 
 if [ ! -d $NEOVIM_DIR ];then
   git clone https://github.com/neovim/neovim $NEOVIM_DIR
+else
+  pushd $NEOVIM_DIR
+  git pull
+  popd
 fi
 
 pushd $NEOVIM_DIR
-git checkout stable
+git checkout $NEOVIM_VERSION
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 popd
