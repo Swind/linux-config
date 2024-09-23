1cddf7aa2e8ef6b362a5f7eeffe4050bb2c8f541
swind <swind@cloudmosa.com>
2024-09-23 16:14:52 +0800

    update community packs and add new ones

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -10,13 +10,15 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.ansible" },
+  -- { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.cpp" },
   -- Completion
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 
