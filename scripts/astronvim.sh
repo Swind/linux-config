8e5a04a47f5d159bd088c6976c26269e27b0753d
Swind <swind@cloudmosa.com>
2025-06-04 00:31:56 +0800

    Remove unused files

--- scripts/astronvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ASTRONVIM_PATH=$HOME/.myconfig/nvim
-#
-# rm -rf $ASTRONVIM_PATH
-#
-# git clone https://github.com/AstroNvim/AstroNvim $HOME/.myconfig/nvim
-# ln -s $HOME/.myconfig/nvim_user $HOME/.myconfig/nvim/lua/user
