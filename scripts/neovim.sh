149846f503d7d557e16dd4cd7c6333f3dfa9ade4
Swind <swind@cloudmosa.com>
2025-06-04 00:30:10 +0800

    feat(neovim): update install script for v0.11.2 and deps
    - Bump Neovim version to v0.11.2.
    - Add btm and luarocks to apt dependencies.
    - Install tree-sitter-cli globally via npm.
    - Improve quoting and directory checks for robustness.
    - Use 'pushd'/'popd' with error handling.

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 echo "neovim: build prerequisites"
-sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen xclip
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ \
+  pkg-config unzip curl doxygen xclip btm luarocks
+npm install -g tree-sitter-cli
 
 TARGET_DIR=$HOME/Software
 NEOVIM_DIR=$TARGET_DIR/neovim
-NEOVIM_VERSION=v0.10.1
+NEOVIM_VERSION=v0.11.2
 
-mkdir -p $TARGET_DIR
+mkdir -p "$TARGET_DIR"
 
-if [ ! -d $NEOVIM_DIR ];then
-  git clone https://github.com/neovim/neovim $NEOVIM_DIR
+if [ ! -d "$NEOVIM_DIR" ]; then
+  git clone https://github.com/neovim/neovim "$NEOVIM_DIR"
 else
-  pushd $NEOVIM_DIR
+  pushd "$NEOVIM_DIR" || exit
   git pull
-  popd
+  popd || exit
 fi
 
-pushd $NEOVIM_DIR
+pushd "$NEOVIM_DIR" || exit
 git pull
 git checkout $NEOVIM_VERSION
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-popd
+popd || exit
 
 #sudo apt-get install software-properties-common
 #sudo add-apt-repository ppa:neovim-ppa/unstable
