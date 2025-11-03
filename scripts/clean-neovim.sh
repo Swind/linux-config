863c6cbe68477e47c75bdc78a13095f70e069ec1
Swind <swind@cloudmosa.com>
2025-11-03 02:52:02 +0000

    Add clean-neovim.sh

---
+++ scripts/clean-neovim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
+
+rm -rf ~/.local/share/nvim.bak
+rm -rf ~/.local/state/nvim.bak
+rm -rf ~/.cache/nvim.bak
