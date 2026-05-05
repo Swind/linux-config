570a32612f86620d735663307a49f34f27969daa
Swind <swind@cloudmosa.com>
2026-05-05 16:20:47 +0800

    Add script for obsidian opencode skills

---
+++ scripts/opencode/obsidian-skills.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npx skills add https://github.com/kepano/obsidian-skills --skill obsidian-markdown --project
+npx skills add https://github.com/kepano/obsidian-skills --skill obsidian-bases --project
+npx skills add https://github.com/kepano/obsidian-skills --skill obsidian-cli --project
+npx skills add https://github.com/kepano/obsidian-skills --skill json-canvas --project
+npx skills add https://github.com/kepano/obsidian-skills --skill defuddle --project
+npx skills add https://github.com/axtonliu/axton-obsidian-visual-skills/ --skill excalidraw-diagram --project
+npx skills add https://github.com/axtonliu/axton-obsidian-visual-skills/ --skill mermaid-visualizer --project
+npx skills add https://github.com/axtonliu/axton-obsidian-visual-skills/ --skill obsidian-canvas-creator --project
